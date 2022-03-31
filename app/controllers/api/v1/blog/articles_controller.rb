1f0c8481420619dd3063eebcecc4558f8c2aaa13
attt <ttt3pu@gmail.com>
2022-04-01 00:12:52 +0900

    👷 Chore: Resolve rubocop todo

--- app/controllers/api/v1/blog/articles_controller.rb
+++ app/controllers/api/v1/blog/articles_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Api::V1::Blog::ArticlesController < ApplicationController
   def index
     articles = BlogArticle.all
--- test/controllers/blog/articles_controller_test.rb
+++ test/controllers/blog/articles_controller_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "test_helper"
 
 class Blog::ArticlesControllerTest < ActionDispatch::IntegrationTest
