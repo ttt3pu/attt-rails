8fe3b36c7629486801fc5417b29afd70b2cf7214
attt <ttt3pu@gmail.com>
2022-04-20 00:10:44 +0900

    ✨ Feat: Add schema and rspec `/api/v1/blog/articles/:slug`

--- spec/requests/api/v1/blog/articles_spec.rb
+++ spec/requests/api/v1/blog/articles_spec.rb
@@ -3,13 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Blog::Articles', type: :request do
-  describe 'GET /api/v1/blog/articles' do
+  describe 'GET #index' do
     before do
-      create_list(:blog_article, 10)
+      blog_articles = create_list(:blog_article, 10)
+      get '/api/v1/blog/articles'
     end
 
-    it '取得できる' do
-      get api_v1_blog_articles_path
+    it '返却形式が正しいこと' do
+      assert_response_schema_confirm(200)
+    end
+  end
+
+  describe 'GET #show' do
+    before do
+      blog_articles = create_list(:blog_article, 10)
+      get "/api/v1/blog/articles/#{blog_articles[0].slug}"
+    end
+
+    it '返却形式が正しいこと' do
       assert_response_schema_confirm(200)
     end
   end
