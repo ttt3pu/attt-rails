fad3b870587e28bd5f6b15088a81e06ee537683e
attt <ttt3pu@gmail.com>
2022-04-27 00:09:54 +0900

    ✅ Test: `/api/v1/blog/articles/create`

--- spec/requests/api/v1/blog/articles_spec.rb
+++ spec/requests/api/v1/blog/articles_spec.rb
@@ -24,4 +24,32 @@ RSpec.describe 'Api::V1::Blog::Articles', type: :request do
       assert_response_schema_confirm(200)
     end
   end
+
+  # TODO: isPublished
+  describe 'POST #create' do
+    slug = Faker::Internet.slug
+    title = Faker::Lorem.word
+    content = Faker::Markdown.random
+
+    before do
+      post "/api/v1/blog/articles/create", params: {
+        isPublished: Faker::Boolean.boolean,
+        slug: slug,
+        title: title,
+        content: content,
+      }
+    end
+
+    it '返却形式とリクエスト形式が正しいこと' do
+      assert_request_schema_confirm
+      assert_response_schema_confirm(204)
+    end
+
+    it 'リクエストした内容でBlogArticleがつくられていること' do
+      blogArticle = BlogArticle.find_by!(slug: slug)
+      expect(blogArticle.slug).to eq slug
+      expect(blogArticle.title).to eq title
+      expect(blogArticle.content).to eq content
+    end
+  end
 end
